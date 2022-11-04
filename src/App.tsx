e7ac92b302e03c381fdc0528b3f42a1ac6e44843
Gosh95 <goshk1995@gmail.com>
2022-11-05 02:56:27 +0900

    feat: 헬스체크

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
 
 export default App;
